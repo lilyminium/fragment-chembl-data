0b472a52d0699e32fb560e141446390060317568
Lily Wang <lily@mdanalysis.org>
2025-04-08 17:31:58 +1000

    tmp

--- 02_fragment/run.sh
+++ 02_fragment/run.sh
@@ -3,8 +3,8 @@
 #SBATCH -p standard
 #SBATCH -t 1-00:00:00
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=128GB
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=32GB
 #SBATCH --account dmobley_lab
 #SBATCH --export ALL
 #SBATCH --constraint=fastscratch
@@ -25,7 +25,12 @@ conda activate yammbs
 #     -o fragments/chembl-35-fragments-single.smi     \
 #     -np 16
 
-python combine-single-fragments.py                 \
+# python combine-single-fragments.py                 \
+#     -i fragments/chembl-35-fragments-single.smi     \
+#     -o output/chembl-35-fragments-combined.smi    \
+#     -np 32
+
+python combine-fragments-intermediate.py                 \
     -i fragments/chembl-35-fragments-single.smi     \
     -o output/chembl-35-fragments-combined.smi    \
-    -np 32
+    -np 64
